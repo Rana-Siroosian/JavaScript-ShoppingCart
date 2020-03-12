5c3633f72762b790afc12745ee515918d3463bc9
Rana-Siroosian <rana.siroosian@gmail.com>
2020-03-12 00:48:31 -0400

    deleted some codes

--- WebContent/script.js
+++ WebContent/script.js
@@ -32,25 +32,7 @@ for (let button of buttons){
 let checkOut = document.getElementById("checkOut");
 checkOut.addEventListener("click", (event) =>{
 	
-//	let header1 = document.getElementById("header1");
-//	let p = document.createElement("p");
-//	header1.appendChild(p);
-//	header1.innerText="Items";
-//	
-//	let header2 = document.getElementById("header2");
-//	let p1 = document.createElement("p");
-//	header2.appendChild(p1);
-//	header2.innerText="Price";
-	
-	
-	
-//	let col = document.getElementById("col1");
-//	let p1 = document.createElement("p");
-//	col.appendChild(p1);
-//	
-//	let col1 = document.getElementById("col2");
-//	let p2 = document.createElement("p");
-//	col1.appendChild(p2);
+
 	
 	let lists= "";
 	let total=0;
@@ -82,7 +64,7 @@ checkOut.addEventListener("click", (event) =>{
 	
 	console.log("total= "+total);
 	console.log("the list: " + lists);
-//	alert("Item you chose: \n" +lists + "Your total is= "+"$"+ total);
+// alert("Item you chose: \n" +lists + "Your total is= "+"$"+ total);
 	
 	
 });
